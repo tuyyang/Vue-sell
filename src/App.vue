fbaa2fd409899bb00bd88a88986224a9c646adfb
tuyyang <46390022+tuyyang@users.noreply.github.com>
2019-12-26 22:13:16 +0800

    finish tab

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div id="app">
     <v-header :seller="seller"></v-header>
+    <div class="tab-wrapper">
+      <tab :tabs="tabs"></tab>
+    </div>
   </div>
 </template>
 
 <script>
 import VHeader from 'components/v-header/v-header'
+import Goods from 'components/goods/goods'
+import Ratings from 'components/ratings/ratings'
+import Seller from 'components/seller/seller'
+import Tab from 'components/tab/tab'
 import {getSeller} from 'api/index'
 
 export default {
@@ -13,6 +20,33 @@ export default {
   data() {
     return {
       seller: {}
+    }
+  },
+  computed: {
+    tabs() {
+      return [
+        {
+          label: '商品',
+          component: Goods,
+          data: {
+            seller: this.seller
+          }
+        },
+        {
+          label: '评价',
+          component: Ratings,
+          data: {
+            seller: this.seller
+          }
+        },
+        {
+          label: '商家',
+          component: Seller,
+          data: {
+            seller: this.seller
+          }
+        }
+      ]
     }
   },
   created() {
@@ -26,9 +60,18 @@ export default {
     }
   },
   components: {
-    VHeader
+    VHeader,
+    Tab
   }
 }
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+  #app
+    .tab-wrapper
+      position fixed
+      top 136px
+      left 0
+      right 0
+      bottom 0
+</style>
